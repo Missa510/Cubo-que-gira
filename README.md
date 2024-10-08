6badf6ef088574aaadbed449f47a3dd7226d7362
Santiago Quintero <sjqv05@gmail.com>
2024-10-08 13:05:55 -0500

    Audio corregio

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Es una página de, literalmente, un cubo que gira
 
+## version 1.0
+La músiquita se repoduce cuando oprimes el botón
+
 > Missa510 ~ Octubre del 2024
--- script.js
+++ script.js
@@ -1,43 +1,41 @@
-import * as THREE from 'three';     
+import * as THREE from 'three';
 
 // Creamos la escena
 const escena = new THREE.Scene()
 // Creamos la camara
 const camara = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-// Renderisamos la camara y la escena
+// Creamos el renderizador
 const render = new THREE.WebGLRenderer()
-// Para poner musiquita
-const escuchador = new THREE.AudioListener()
-const sonido = new THREE.Audio(escuchador)
-escena.add(escuchador)
+// Creamos el componente de listener
 
 function Iniciar() {
-
-    const puntos = []
-    const escala = 1.5, escala_cubo = 1.02
-    const velocidad = 0.008
-
+    
     // DOM
     const $ = (el) => document.querySelector(el)
+    const $musiquita = $('#musiquita')
     const $container = $(".container")
     const $button_close = $(".btn_close")
     const $button = $(".btn_full")
-    
+
+    $button.addEventListener("click", togglePantallaCompleta)
+
     $button_close.addEventListener("click", () => {
 
         $container.style.display = "none"
+        $musiquita.volume = 0.8
+        $musiquita.play()
 
     })
-
-    $button.addEventListener("click", togglePantallaCompleta)
 
     function togglePantallaCompleta() {
 
         if (!document.fullscreenElement) {
+
             document.documentElement.requestFullscreen()
             $button.innerText = 'Salir de la pantalla completa'
             render.setSize(window.screen.width, window.screen.height)
         } else {
+
             document.exitFullscreen()
             $button.innerText = 'Pantalla completa'
             render.setSize(window.screen.availWidth, window.screen.availHeight)
@@ -45,19 +43,30 @@ function Iniciar() {
 
     }
 
+    // Seteamos la musiquita
+
+    // Fin del Los elementos del DOM
+
+    // Constantes de inicio
+    const puntos = []
+    const escala = 1.5, escala_cubo = 1.02
+    const velocidad = 0.0077
+
     // Ajustamos el tamaño de la ventana
     render.setSize(window.innerWidth, window.innerHeight)
+    // Seteamos la funcion de animar
     render.setAnimationLoop(animar)
+    // Crea el elemento canvas para el render
     document.body.appendChild(render.domElement)
 
     // Creamos un cubo. 1: definimos la geometría
     const geometria = new THREE.BoxGeometry(escala_cubo, escala_cubo, escala_cubo)
+
     // 2: Definimos el material
     const material = new THREE.MeshStandardMaterial({
         color: 0xffffff,
 
     })
-
     const material_linea = new THREE.LineDashedMaterial({
         color: 0xffffff
     })
@@ -77,7 +86,7 @@ function Iniciar() {
     puntos.push(new THREE.Vector3(0, 0, -escala))
     puntos.push(new THREE.Vector3(0, -escala, 0))
 
-    // Intersectar puntos
+    // Intersectar puntos faltantes
     puntos.push(new THREE.Vector3(0, 0, -escala))
     puntos.push(new THREE.Vector3(-escala, 0, 0))
     puntos.push(new THREE.Vector3(0, 0, -escala))
@@ -85,14 +94,14 @@ function Iniciar() {
     puntos.push(new THREE.Vector3(0, 0, escala))
 
 
+    // Definimos la geometria del rombo
     const geometria_linea = new THREE.BufferGeometry().setFromPoints(puntos)
 
+    // Creamos las luces que iluminan al cubo
     const luz_pricipal = new THREE.AmbientLight(0xefefef, 0.1)
     const luz1 = new THREE.DirectionalLight(0xff2f2f, 1)
     const luz2 = new THREE.DirectionalLight(0x4f4ff5, 1)
     const luz3 = new THREE.DirectionalLight(0x23ff44, 1)
-
-    const cargador = new THREE.AudioLoader()
 
     // 3: Creamos el objeto Mesh (cubo)
     const cubo = new THREE.Mesh(geometria, material)
@@ -101,19 +110,12 @@ function Iniciar() {
     // Seteamos la camara
     camara.position.z = 3
 
-    // Añadimos los objetos a la escena
-    luz1.position.set(4, 4, 0)
+    // Posicionamos las luces en la escena
+    luz1.position.set(4, 0, 0)
     luz2.position.set(0, 4, 0)
-    luz3.position.set(0, -4, 4)
+    luz3.position.set(0, 0, 4)
 
-    // Seteamos la musiquita
-    cargador.load("audio/the-approaching-night.mp3", (buffer) => {
-        sonido.setBuffer(buffer)
-        sonido.setLoop(true)
-        sonido.setVolume(0.8)
-        sonido.play()
-    })
-
+    // Añadimos los elementos a la escena
     escena.add(luz_pricipal)
     escena.add(luz1)
     escena.add(luz2)
